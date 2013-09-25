f647287899e3abc0c3ff5166cf0c8dab10dd493b
Xiaolong Tang <x.long@InfoBeyonds.com>
2013-09-24 23:46:14 -0400

    Search button

--- trunk/TAE_SDK/htdocs/tshop/modules/tshop-um-inline-header/tshop-um-inline-header.php
+++ trunk/TAE_SDK/htdocs/tshop/modules/tshop-um-inline-header/tshop-um-inline-header.php
@@ -1,135 +1,159 @@
 
-<div class="tb-module tshop-um tshop-um-inline-header" 
+<div class="tb-module tshop-um tshop-um-inline-header"
 	style="height: 150px;width: 950px;margin: auto;font-size: 14px">
 	<div class="top-notifications" style="padding-top: 5px; height: 25px;">
 		<div class="news-wrap" style="height: 25px; float: left;">
-			<div class="news-ico" 
+			<div class="news-ico"
 				style="width: 22px;
-				       height: 20px;
-				       float: left;
-				       margin-left: 1em;
-				       overflow: hidden;
-				       background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 2px -68px" >
-		   </div>
-				<div class="J_TWidget"
-					data-widget-type="Slide"
-					data-widget-config="{
-					'contentCls':'my-news-items',
-					'hasTriggers':false,
-					'effect':'scrolly',
-					'easing':'easeOutStrong',
-					'interval':5,'duration':0.5}"
-					style="width: 300px;float: left;line-height: 15px;height: 18px; overflow: hidden;">
-					<ul class="my-news-items" style="list-style: none;margin: 0 2em;">
-						<li style="text-align: left;line-height: 18px">
-						<a class="first-trigger" href="#" target="_blank" style="color:#FFF;">今日更新!</a></li>
-							</ul></div>
-						</div>
-
-
-
-						<div style="float: right;margin-right: 1em">
-							<a class="J_TWidget" 
-								href="#" target="_blank" 
-								style="display: inline-block;
-								       margin-left: 8px;
-								       padding-left: 15px;
-								       line-height: 18px;
-								       float: left;
-								       color: #fff;
-								       background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 0 4px">我的订单</a>
-							<a class="J_CartPluginTrigger" 
-								href="#" target="_blank" 
-								style="display: inline-block;
-								       margin-left: 8px;
-								       padding-left: 15px;
-								       line-height: 18px;
-								       float: left;
-								       color: #fff;
-								       background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 0 -20px">购物车</a>
-							<a class="J_TWidget" 
-								href="#" 
-								target="_blank" 
-								title="喜欢我就收藏我吧！亲" 
-								style="display: inline-block;
-								       margin-left: 8px;
-								       padding-left: 15px;
-								       line-height: 18px;
-								       float: left;
-								       color: #fff;
-								       background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 0 -43px">收藏本店</a>
-						</div>
-					</div>
-
-
-					<div class="main-area" style="clear: both;display: block;height: 90px;margin: 0;padding: 0">
-						<div class="logo" style="margin-top: 2px;float: left;width: 200px">
-							<a href="#" target="http://igoody.taobao.com">
-							<img src="http://img02.taobaocdn.com/imgextra/i2/22629828/T2SA42XmpdXXXXXXXX_!!22629828.jpg" alt="Oreya卖家"/></a>
-						</div>
-
-						<div class="certificates" style="margin-top: 4px;float: right;width: 340px;height: 86px">
-							<div class="taobao-quanqiugou" style="float: left;margin-left: 10px;padding-left: 9px;border-left: 1px solid #808080">
-								<div class="image">
-									<a href="#" target="_blank">
-									<img src="http://img02.taobaocdn.com/tps/i2/T1aT2qXklpXXcCXlfb-25-25.png" alt="全球购"/></a>
-								</div>
-								<div class="text" style="text-align: center;font-weight: bold;font-size: 23px;color: #fff">
-									<span>优质卖家</span>
-								</div>
-							</div>
-							<div class="vertical-line">
-							</div>
-							<div class="bookmark-shop" style="float: left;margin-left: 10px;line-height: 0;border-left: 1px solid #808080">
-								<a class="myfav J_TWidget" href="#" target="_blank">
-								<img src="assets/images/bookmark-shop.jpg" alt="收藏店铺" height="83px"/></a>
-							</div>
-
-						</div>
-					</div>
-
-					<div class="top-nav" style="height: 30px;font-size: 16px;font-weight: bold;clear: both">
-						<ul class="menu-list"><li class="menu" style="float: left">
-							<a class="link home-link" href="http://igoody.taobao.com/index.htm" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;padding: 0 1em"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">首页</span></a>
-						</li>
-						<li class="menu" style="float: left">
-							<a class="link all-cats-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">所有产品</span></a>
-						</li>
-						<li class="menu" style="float: left">
-							<a class="link all-cats-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="color: red;display: inline-block;line-height: 30px;height: 30px">特惠产品</span></a>
-						</li>
-						<li class="menu" style="float: left">
-							<a class="link new-arrival-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">最新款式</span></a>
-						</li>
-						<li class="menu" style="float: left">
-							<a class="link season-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">季节款</span></a>
-						</li>
-						<li class="menu" style="float: left">
-							<a class="link woman-section-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">女士专区</span></a>
-						</li>
-						<li class="menu" style="float: left">
-							<a class="link man-section-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">男士专区</span></a>
-						</li>
-						<li class="menu" style="float: left">
-							<a class="link about-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">关于我们</span></a>
-						</li>
+				height: 20px;
+				float: left;
+				margin-left: 1em;
+				overflow: hidden;
+				background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 2px -68px" >
+			</div>
+			<div class="J_TWidget"
+				data-widget-type="Slide"
+				data-widget-config="{
+				'contentCls':'my-news-items',
+				'hasTriggers':false,
+				'effect':'scrolly',
+				'easing':'easeOutStrong',
+				'interval':5,'duration':0.5}"
+				style="width: 300px;float: left;line-height: 15px;height: 18px; overflow: hidden;">
+				<ul class="my-news-items" style="list-style: none;margin: 0 2em;">
+					<li style="text-align: left;line-height: 18px">
+					<a class="first-trigger" href="#" target="_blank" style="color:#FFF;">今日更新!</a></li>
 					</ul></div>
 				</div>
 
-				<div class="J_TWidget hidden" data-widget-type="Popup" data-widget-config="{
-					'trigger':'.first-trigger',
-					'align':{
-					'node':'.first-trigger',
-					'offset':[350,60],
-					'points':['cr','cc']
-					}
-					}">
-					<div style="background-color: #ABABAB; height: 90px; width: 950px; border: 1px; ">
-						<div class="description" style="font-size: 15px; color: blue; position: relative; top: 0; left: 0;" >
-							<h1 style="padding-left: 10px;padding-top: 10px;">
-							<span>Coach 鞋子</span>								
-							</h1>
 
+
+				<div style="float: right;margin-right: 1em">
+					<a class="J_TWidget"
+						href="#" target="_blank"
+						style="display: inline-block;
+						margin-left: 8px;
+						padding-left: 15px;
+						line-height: 18px;
+						float: left;
+						color: #fff;
+					background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 0 4px">我的订单</a>
+					<a class="J_CartPluginTrigger"
+						href="#" target="_blank"
+						style="display: inline-block;
+						margin-left: 8px;
+						padding-left: 15px;
+						line-height: 18px;
+						float: left;
+						color: #fff;
+					background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 0 -20px">购物车</a>
+					<a class="J_TWidget"
+						href="#"
+						target="_blank"
+						title="喜欢我就收藏我吧！亲"
+						style="display: inline-block;
+						margin-left: 8px;
+						padding-left: 15px;
+						line-height: 18px;
+						float: left;
+						color: #fff;
+					background: url('http://img.taobao.com/L1/142/405832076/modules/tshop-um-xsj-banner/assets/images/s-ico.gif') no-repeat 0 -43px">收藏本店</a>
+				</div>
+			</div>
+
+
+			<div class="main-area" style="clear: both;display: block;height: 90px;margin: 0;padding: 0">
+				<div class="logo" style="margin-top: 2px;float: left;width: 200px">
+					<a href="#" target="http://igoody.taobao.com">
+					<img src="http://img02.taobaocdn.com/imgextra/i2/22629828/T2SA42XmpdXXXXXXXX_!!22629828.jpg" alt="Oreya卖家"/></a>
+				</div>
+
+				<div class="search-form" style="margin-left: 20px; margin-top: 20px; float: left; height: 50px; vertical-align: middle; line-height: 30px;">
+					<form name="SearchForm" action="http://igoody.taobao.com/?search=y&amp;scene=taobao_shop" method="post">
+						<input class="search-form-input" style="display: block; float: left; width: 300px; font-size: 20px; line-height: 30px;" size="18" name="keyword" value="经典女包" type="text">
+						<button type="submit" class="search-form-btn" style="display: block; color:#fff;
+							padding:3px 20px 4px; float: left;
+							margin-left: 3px;
+							font-size: 14px; 
+							line-height: 25px;
+							text-shadow:0 1px 0;
+							border-top-color: #2c8ed1;
+							border-bottom-color: #0d5b97;
+							-webkit-transition:border-color .218s;-moz-transition:border .218s;-o-transition:border-color .218s;
+							transition:border-color .218s;
+							background:#2181cf;
+							background:-webkit-gradient(linear,left top,left bottom,from(#37a3eb),to(#2181cf));
+							background:-moz-linear-gradient(linear,linear,left top,left bottom,from(#37a3eb),to(#2181cf));
+							border:solid 1px #1cb473;
+							border-radius:2px;
+						-webkit-border-radius:2px;-moz-border-radius:2px;cursor:pointer;height:auto;width:auto;overflow:visible;">搜索</button>
+						<input name="userId" value="" type="hidden">
+						<input name="shopId" value="33073918" type="hidden">
+						<input name="view_type" value="" type="hidden">
+						<input name="order_type" value="" type="hidden">
+						<input name="search" value="y" type="hidden">
+					</form>
+				</div>
+
+				<div class="certificates" style="margin-top: 4px;float: right;width: 300px;height: 86px">
+					<div class="taobao-quanqiugou" style="float: left;margin-left: 10px;padding-left: 9px;">
+						<div class="image">
+							<a href="#" target="_blank">
+							<img src="http://img01.taobaocdn.com/tps/i1/T1NPnkXb8tXXaDCIDc-150-45.png" alt=""/></a>
+						</div>
+						<div class="text" style="text-align: center;font-weight: bold;font-size: 20px;color: #000">
+							<span>全球购优质卖家</span>
 						</div>
 					</div>
 				</div>
+
+			</div>
+
+
+
+
+			<div class="top-nav" style="height: 30px;font-size: 16px;font-weight: bold;clear: both">
+				<ul class="menu-list"><li class="menu" style="float: left">
+					<a class="link home-link" href="http://igoody.taobao.com/index.htm" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;padding: 0 1em"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">首页</span></a>
+				</li>
+				<li class="menu" style="float: left">
+					<a class="link all-cats-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">所有产品</span></a>
+				</li>
+				<li class="menu" style="float: left">
+					<a class="link all-cats-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="color: red;display: inline-block;line-height: 30px;height: 30px">特惠产品</span></a>
+				</li>
+				<li class="menu" style="float: left">
+					<a class="link new-arrival-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">最新款式</span></a>
+				</li>
+				<li class="menu" style="float: left">
+					<a class="link season-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">季节款</span></a>
+				</li>
+				<li class="menu" style="float: left">
+					<a class="link woman-section-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">女士专区</span></a>
+				</li>
+				<li class="menu" style="float: left">
+					<a class="link man-section-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">男士专区</span></a>
+				</li>
+				<li class="menu" style="float: left">
+					<a class="link about-link" href="#" style="text-decoration: none;color: #000;display: inline-block;margin: 0 1em;"><span class="title" style="display: inline-block;line-height: 30px;height: 30px">关于我们</span></a>
+				</li>
+			</ul></div>
+		</div>
+
+		<div class="J_TWidget hidden" data-widget-type="Popup" data-widget-config="{
+			'trigger':'.first-trigger',
+			'align':{
+			'node':'.first-trigger',
+			'offset':[350,60],
+			'points':['cr','cc']
+			}
+			}">
+			<div style="background-color: #ABABAB; height: 90px; width: 950px; border: 1px; ">
+				<div class="description" style="font-size: 15px; color: blue; position: relative; top: 0; left: 0;" >
+					<h1 style="padding-left: 10px;padding-top: 10px;">
+						<span>Coach 鞋子</span>
+					</h1>
+
+				</div>
+			</div>
+		</div>
