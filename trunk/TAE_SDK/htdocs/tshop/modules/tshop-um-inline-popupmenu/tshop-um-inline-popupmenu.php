36b2fdd886c93bddb07c21af5db756fd0b86e593
Xiaolong Tang <x.long@InfoBeyonds.com>
2013-09-25 01:50:58 -0400

    branch menu

---
+++ trunk/TAE_SDK/htdocs/tshop/modules/tshop-um-inline-popupmenu/tshop-um-inline-popupmenu.php
@@ -0,0 +1,28 @@
+
+<div class="tb-module tshop-um tshop-um-inline-popupmenu">
+
+	<div class="J_TWidget hidden" data-widget-type="Popup"
+		data-widget-config="{'trigger':'.brand-section-link','align':{'node':'.brand-section-link', 'offset':[0,0], 'points':['bl','tl']}}">
+		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
+			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
+				<ul style="padding: 5px 10px; text-decoration: none;">
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Coach</a> </li>					
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Michael kors</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Rebecca Minkoff</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Furla</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Crabtree Evelyn</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Levis</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">CK</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Ralph Lauren</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Ugg</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Nike</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">New balance</a> </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	
+</div>
