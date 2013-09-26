947b5898b2091f62d7416adefaefcf23b7eecf61
Xiaolong Tang <x.long@InfoBeyonds.com>
2013-09-26 03:36:45 -0400

    work in middle

--- trunk/TAE_SDK/htdocs/tshop/modules/tshop-um-inline-popupmenu/tshop-um-inline-popupmenu.php
+++ trunk/TAE_SDK/htdocs/tshop/modules/tshop-um-inline-popupmenu/tshop-um-inline-popupmenu.php
@@ -2,11 +2,109 @@
 <div class="tb-module tshop-um tshop-um-inline-popupmenu">
 
 	<div class="J_TWidget hidden" data-widget-type="Popup"
+		data-widget-config="{'trigger':'.new-arrival-link','align':{'node':'.new-arrival-link', 'offset':[0,0], 'points':['bl','tl']}}">
+		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
+			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
+				<ul style="padding: 5px 10px; text-decoration: none;">
+					<li style="margin-bottom: 2px"><a style="color: black" href="http://igoody.taobao.com/category-798643869.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=798644201&parentCatName=%D0%C2%C6%B7%C9%CF%CA%D0&catName=13%C4%EA09%D4%C2%C9%CF%D0%C2#bd" target="_self" title="9月上新">9月上新</a> </li>					
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="J_TWidget hidden" data-widget-type="Popup"
+		data-widget-config="{'trigger':'.bag-cats-link','align':{'node':'.bag-cats-link', 'offset':[0,0], 'points':['bl','tl']}}">
+		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
+			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
+				<ul style="padding: 5px 10px; text-decoration: none;">
+					<li style="margin-bottom: 2px"><a style="color: black" href="http://igoody.taobao.com/category-799092495.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092494&parentCatName=%C5%AE%B0%FC%C4%D0%B0%FC&catName=%C5%AE%CA%BF%C7%AE%B0%FC%C7%AE%BC%D0#bd" target="_self" title="女士钱包钱夹">女士钱包钱夹</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799092496.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092494&parentCatName=%C5%AE%B0%FC%C4%D0%B0%FC&catName=%C5%AE%CA%BF%CA%D6%BB%FA%CC%D7%CA%D6%BB%FA%BF%C7#bd" target="_self" title="女士手机套手机壳">女士手机套手机壳</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799092497.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092494&parentCatName=%C5%AE%B0%FC%C4%D0%B0%FC&catName=%C5%AE%CA%BF%B5%A5%BC%E7%B0%FC%CA%D6%CC%E1%B0%FC%D0%B1%BF%E6%B0%FC#bd" target="_self" title="女士单肩包手提包斜跨包">女士单肩包手提包斜跨包</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799092498.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092494&parentCatName=%C5%AE%B0%FC%C4%D0%B0%FC&catName=%C4%D0%CA%BF%C7%AE%B0%FC%C7%AE%BC%D0#bd" target="_self" title="男士钱包钱夹">男士钱包钱夹</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799092499.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092494&parentCatName=%C5%AE%B0%FC%C4%D0%B0%FC&catName=%C4%D0%CA%BF%CA%D6%BB%FA%CC%D7%CA%D6%BB%FA%BF%C7#bd" target="_self" title="男士手机套手机壳">男士手机套手机壳</a> </li>
+					<li style="margin-bottom: 2px"><a style="color: black" href="http://igoody.taobao.com/category-799092500.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092494&parentCatName=%C5%AE%B0%FC%C4%D0%B0%FC&catName=%C4%D0%CA%BF%B5%A5%BC%E7%B0%FC%CA%D6%CC%E1%B0%FC%D0%B1%BF%E6%B0%FC#bd" target="_self" title="男士单肩包手提包斜跨包">男士单肩包手提包斜跨包</a> </li>			
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="J_TWidget hidden" data-widget-type="Popup"
+		data-widget-config="{'trigger':'.shoe-cats-link','align':{'node':'.shoe-cats-link', 'offset':[0,0], 'points':['bl','tl']}}">
+		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
+			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
+				<ul style="padding: 5px 10px; text-decoration: none;">
+					<li style="margin-bottom: 2px"><a style="color: black" href="http://igoody.taobao.com/category-799092928.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092927&parentCatName=%C5%AE%D0%AC%C4%D0%D0%AC&catName=%C5%AE%D0%AC#bd" target="_self" title="女鞋">女鞋</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799092929.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092927&parentCatName=%C5%AE%D0%AC%C4%D0%D0%AC&catName=%C4%D0%D0%AC#bd" target="_self" title="男鞋">男鞋</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799092930.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092927&parentCatName=%C5%AE%D0%AC%C4%D0%D0%AC&catName=%C5%AE%CD%AF%D0%AC#bd" target="_self" title="女童鞋">女童鞋</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799092931.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799092927&parentCatName=%C5%AE%D0%AC%C4%D0%D0%AC&catName=%C4%D0%CD%AF%D0%AC#bd" target="_self" title="男童鞋">男童鞋</a> </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="J_TWidget hidden" data-widget-type="Popup"
+		data-widget-config="{'trigger':'.belt-cats-link','align':{'node':'.belt-cats-link', 'offset':[0,0], 'points':['bl','tl']}}">
+		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
+			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
+				<ul style="padding: 5px 10px; text-decoration: none;">
+					<li style="margin-bottom: 2px"><a style="color: black" href="http://igoody.taobao.com/category-799093120.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799093119&parentCatName=%C6%A4%B4%F8%D1%FC%B4%F8+%C5%E4%BC%FE&catName=%C5%AE%CA%BF%C6%A4%B4%F8%D1%FC%B4%F8#bd" target="_self" title="女士皮带腰带">女士皮带腰带</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799093121.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799093119&parentCatName=%C6%A4%B4%F8%D1%FC%B4%F8+%C5%E4%BC%FE&catName=%C5%AE%CA%BF%CB%BF%BD%ED%CE%A7%BD%ED#bd" target="_self" title="女士丝巾围巾">女士丝巾围巾</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799093122.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799093119&parentCatName=%C6%A4%B4%F8%D1%FC%B4%F8+%C5%E4%BC%FE&catName=%C5%AE%CA%BF%C3%B1%D7%D3#bd" target="_self" title="女士帽子">女士帽子</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/category-799093123.htm?spm=0.0.0.0.HVE2Ci&search=y&parentCatId=799093119&parentCatName=%C6%A4%B4%F8%D1%FC%B4%F8+%C5%E4%BC%FE&catName=%C4%D0%CA%BF%C6%A4%B4%F8%D1%FC%B4%F8#bd" target="_self" title="男士皮带腰带">男士皮带腰带</a> </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="J_TWidget hidden" data-widget-type="Popup"
+		data-widget-config="{'trigger':'.woman-section-link','align':{'node':'.woman-section-link', 'offset':[0,0], 'points':['bl','tl']}}">
+		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
+			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
+				<ul style="padding: 5px 10px; text-decoration: none;">
+					<li style="margin-bottom: 2px"><a style="color: black" href="http" target="_self" title="服装">服装</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="钱包钱夹">钱包钱夹</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="手机套手机壳">手机套手机壳</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="单肩包手提包斜跨包">单肩包手提包斜跨包</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="腰带皮带">腰带皮带</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="鞋子">鞋子</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="丝巾围巾">丝巾围巾</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="帽子">帽子</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="眼睛太阳镜">眼睛太阳镜</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="护肤品">护肤品</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">New</a> </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	
+		<div class="J_TWidget hidden" data-widget-type="Popup"
+		data-widget-config="{'trigger':'.woman-section-link','align':{'node':'.woman-section-link', 'offset':[0,0], 'points':['bl','tl']}}">
+		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
+			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
+				<ul style="padding: 5px 10px; text-decoration: none;">
+					<li style="margin-bottom: 2px"><a style="color: black" href="http" target="_self" title="Coach">服装</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">钱包钱夹</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">手机套手机壳</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">单肩包手提包斜跨包</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">腰带皮带</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">鞋子</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">丝巾围巾</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">帽子</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">眼睛太阳镜</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">护肤品</a> </li>
+					<li style="margin-bottom: 2px;"><a style="color: black" href="http" target="_self" title="Coach">New</a> </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="J_TWidget hidden" data-widget-type="Popup"
 		data-widget-config="{'trigger':'.brand-section-link','align':{'node':'.brand-section-link', 'offset':[0,0], 'points':['bl','tl']}}">
 		<div class="popup-content-wrapper clearfix" style="clear: both;margin: auto; width:150px; text-align: left;">
 			<div class="popup-content clearfix" style="clear: both; padding:0;margin:0;border:1px solid #C1C1C1;background:none repeat scroll 0 0 #F6F6F4; font-size: 15px; font-family: sans-serif, helvetica,arial,clean;">
 				<ul style="padding: 5px 10px; text-decoration: none;">
-					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Coach</a> </li>					
+					<li style="margin-bottom: 2px"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Coach</a> </li>
 					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Michael kors</a> </li>
 					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Rebecca Minkoff</a> </li>
 					<li style="margin-bottom: 2px;"><a style="color: black" href="http://igoody.taobao.com/?search=y&keyword=Coach" target="_self" title="Coach">Furla</a> </li>
@@ -21,8 +119,6 @@
 			</div>
 		</div>
 	</div>
-	
-	
-	
-	
+
+
 </div>
